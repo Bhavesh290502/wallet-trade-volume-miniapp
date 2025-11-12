4b7476fd369e7ea7e247eabfcb1b4c72a8e37fde
Bhavesh <bhaveshsinghal2905@gmail.com>
2025-11-12 22:00:49 +0530

    check

---
+++ api/check-env.js
@@ -0,0 +1,12 @@
+// api/check-env.js
+module.exports = async (req, res) => {
+  try {
+    const hasKey = !!process.env.BITQUERY_API_KEY;
+    return res.status(200).json({
+      ok: true,
+      hasBitqueryEnv: hasKey
+    });
+  } catch (err) {
+    return res.status(500).json({ ok: false, error: String(err) });
+  }
+};
